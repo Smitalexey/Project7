e4ae363404e73f94cfcd8c39cee1852be501753b
Aleksey <smit_alexey@mail.ru>
2022-08-28 10:19:01 +0500

    Выдаем число двумерного массива

---
+++ task50/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите размеры массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] massiv = new int[m, n];
+for (int i = 0; i < massiv.GetLength(0); i++)
+{
+    for (int j = 0; j < massiv.GetLength(1); j++)
+        massiv[i, j] = Convert.ToInt32(new Random().Next(0, 100));
+}
+
+for (int i = 0; i < massiv.GetLength(0); i++)
+{
+    for (int j = 0; j < massiv.GetLength(1); j++)
+        Console.Write(massiv[i, j] + "  ");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите координаты");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > m || b > n)
+    Console.WriteLine("такого числа нет");
+else
+{
+    object c = massiv.GetValue(a - 1, b - 1);
+    Console.WriteLine("Искомое число: ");
+    Console.WriteLine(c);
+}
